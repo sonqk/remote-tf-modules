e550b3e09e32e2d1c3322826c4738b3628f72426
sonqk <sonqkcv@gmail.com>
2023-08-05 12:02:11 +0700

    abc

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
-resource "azurerm_resource_group" "vm_rg" {
-  for_each = var.vms
-  name     = each.value.rg_name
-  location = each.value.location
+resource "azurerm_resource_group" "test-rg" {
+  name     = var.rg_name
+  location = var.location
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,6 @@
-variable "vms" {
-  type = map(object({
-    rg_name       = string
-    location      = string
-    vm_size       = string
-    admin_username = string
-    admin_password = string
-  }))
+variable "rg_name" {
+  type = string
+}
+variable "location" {
+  type = string
 }
